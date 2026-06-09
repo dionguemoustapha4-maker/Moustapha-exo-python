89748caaa26052bb05cf911a30380680d555eb9d
MOUSTAPHA DIONGUE <moustaphadiongue@MacBook-Air-de-MOUSTAPHA.local>
2026-06-09 14:46:10 +0200

    correction hello.py

--- hello.py
+++ hello.py
@@ -1,14 +1,4 @@
-print("Bonjour le monde !")
-print("Python", "c'est parti")
-print(2026)
-print(10 + 5)
-prenom = "Mous"
-age = 20
-
-print(prenom)
-print(age)
 prenom = input("Quel est ton prénom ? ")
-print(prenom)
 age = int(input("Ton âge ? "))
-print(age)
+
 print(prenom + ", dans 10 ans tu auras " + str(age + 10) + " ans.")
