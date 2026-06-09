5a707bb473635f826b90e3a5f485b1245cf345bd
MOUSTAPHA DIONGUE <moustaphadiongue@MacBook-Air-de-MOUSTAPHA.local>
2026-06-09 16:07:11 +0200

    exercices listes

--- listes.py
+++ listes.py
@@ -7,3 +7,18 @@ for nombre in nombres:
         plus_grand = nombre
 
 print("Le plus grand nombre est : " + str(plus_grand))
+notes = [12, 15, 8, 19, 14]
+
+total = 0
+for note in notes:
+    total = total + note
+
+moyenne = total / len(notes)
+print("Moyenne : " + str(moyenne))
+
+compteur = 0
+for note in notes:
+    if note > moyenne:
+        compteur = compteur + 1
+
+print("Notes au-dessus de la moyenne : " + str(compteur))
